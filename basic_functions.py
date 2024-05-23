748798fe904600113ebf283fe3372f4aed59d23d
AgnieszkaKulik <165155417+AgnieszkaKulik@users.noreply.github.com>
2024-05-23 19:39:17 +0200

    Add files via upload

---
+++ basic_functions.py
@@ -0,0 +1,51 @@
+import math
+
+expression = ""
+
+def press(character, flag):
+    global expression
+
+    if flag and character.isnumeric() or expression == "ERROR":
+        expression = str(character)
+
+    elif len(expression) == 0 and not character.isnumeric():
+        if str(character) is '-':
+            expression = '-'
+
+    elif len(expression) > 0 and str(character) in ['+', '-', '/', '*', '.']:
+        if expression[-1] in  ['+', '-', '/', '*']:
+            expression = expression[:-1] + str(character)
+        else:
+            expression += str(character)
+
+    else:
+        expression += str(character)
+
+    return expression, 0
+
+def power2_fun():
+    global expression
+
+    value = str(eval(expression)**2)
+    return expression
+
+def sqrt_fun():
+    global expression
+
+    try:
+        expression = str(math.sqrt(eval(expression)))
+        return expression
+
+    except:
+        return "ERROR"
+
+def equal_fun():
+    global expression
+
+    try:
+        expression = str(eval(expression))
+        return expression
+
+    except:
+        expression = ""
+        return "ERROR"
