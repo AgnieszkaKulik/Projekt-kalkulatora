4a101a24e31dc44cdfef471f3d9732f7e141eb7c
AgnieszkaKulik <165155417+AgnieszkaKulik@users.noreply.github.com>
2024-05-25 22:46:20 +0200

    dodanie komentarzy

--- basic_functions.py
+++ basic_functions.py
@@ -1,19 +1,21 @@
 import math
 
 expression = ""
-
+operators = ['+', '-', '/', '*']
+#obsługa po naciśnięciu przycisku
 def press(character, flag):
     global expression
 
+    #sprawdza czy naciśnięty znak po równa się jest cyfrą (tworzy nowe równanie), sprawdza czy jest liczba lub czy jest to błąd
     if flag and character.isnumeric() or expression == "ERROR":
         expression = str(character)
-
+    #sprawdza czy pierwszy znak to minus
     elif len(expression) == 0 and not character.isnumeric():
-        if str(character) is '-':
+        if str(character) == '-':
             expression = '-'
-
-    elif len(expression) > 0 and str(character) in ['+', '-', '/', '*', '.']:
-        if expression[-1] in  ['+', '-', '/', '*']:
+    #sprawdza czy poprzedni znak to operator jeśli tak to zamienia go 
+    elif len(expression) > 0 and str(character) in operators:
+        if expression[-1] in operators:
             expression = expression[:-1] + str(character)
         else:
             expression += str(character)
@@ -22,13 +24,14 @@ def press(character, flag):
         expression += str(character)
 
     return expression, 0
-
+#podnoszenie wpisanego równania do kwadratu
 def power2_fun():
     global expression
 
     value = str(eval(expression)**2)
     return expression
 
+#pierwiastek kwadratowy
 def sqrt_fun():
     global expression
 
@@ -38,7 +41,7 @@ def sqrt_fun():
 
     except:
         return "ERROR"
-
+#wylicza równianie, gdy wykreyje błąd zwraca error
 def equal_fun():
     global expression
 
@@ -49,7 +52,8 @@ def equal_fun():
     except:
         expression = ""
         return "ERROR"
-        
+    
+#sprawdza czy jest w ciągu znaków przecinek jeśli jest to nie pozwala na kolejne wstawienie
 def comma():
     global expression
 
@@ -61,7 +65,7 @@ def comma():
         while i >= 0:
             if expression[i] == '.':
                 return expression
-            if expression[i] in ['+', '-', '/', '*']:
+            if expression[i] in operators:
                 if i == len(expression) -1:
                     expression += '0'
                 break
